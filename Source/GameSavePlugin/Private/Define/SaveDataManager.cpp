180fb1b2840a3d08ac0b05e3d635fc1f34a860f9
Bandit-Empire <731796991@qq.com>
2016-12-27 21:29:50 +0800

    数据结构

---
+++ Source/GameSavePlugin/Private/Define/SaveDataManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameSavePluginPrivatePCH.h"
+#include "SaveDataManager.h"
+
+using namespace std;
+
+USaveDataManager* USaveDataManager::Instance = nullptr;
+USaveDataManager::USaveDataManager()
+{
+
+}
